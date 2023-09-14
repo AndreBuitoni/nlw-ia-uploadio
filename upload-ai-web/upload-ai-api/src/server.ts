8cce7616248d9b7f9972c0e4076b6e72e458a8e8
André Buitoni <a.buitoni@gmail.com>
2023-09-14 19:29:34 -0300

    Finalizei a transcrição do mp3

--- upload-ai-web/upload-ai-api/src/server.ts
+++ upload-ai-web/upload-ai-api/src/server.ts
@@ -1,11 +1,16 @@
+// Importações do Fastify
 import { fastify } from 'fastify'
+
+// Importações das Rotas
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-videos'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3333,
