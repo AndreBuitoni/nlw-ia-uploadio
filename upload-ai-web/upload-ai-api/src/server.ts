25495f42183ba2a1cb8e0004b23c2d2e15adceaf
André Buitoni <a.buitoni@gmail.com>
2023-09-18 17:00:20 -0300

    Criação do Backend Finalizada

--- upload-ai-web/upload-ai-api/src/server.ts
+++ upload-ai-web/upload-ai-api/src/server.ts
@@ -1,5 +1,6 @@
 // Importações do Fastify
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 
 // Importações das Rotas
 import { getAllPromptsRoute } from './routes/get-all-prompts'
@@ -8,6 +9,12 @@ import { createTranscriptionRoute } from './routes/create-transcription'
 import { generateAICompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
+
+// A seguinte configuração determina quais frontends podem fazer solicitação para este servidor.
+// Em ambiente de produção, o origin deveria ser o endereço URL onde está hospedado nosso aplicativo web.
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 // Criação das Rotas
 app.register(getAllPromptsRoute)
