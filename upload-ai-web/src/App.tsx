7da9964520c12bfeba5f47878f671d033a6f2751
André Buitoni <a.buitoni@gmail.com>
2023-10-10 01:00:34 -0300

    Carreguei os prompts do backend na tela do usuário

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,5 +1,7 @@
 // Importações Externas
+import { useState } from 'react'
 import { Github, Wand2 } from 'lucide-react'
+import { useCompletion } from 'ai/react'
 
 // Importações Internas - components/ui (elementos de componentes)
 import { Button } from "./components/ui/button";
@@ -12,18 +14,23 @@ import { Slider } from './components/ui/slider';
 // Importações Internas - Componentes Completos
 import { VideoInputForm } from './components/video-input-form';
 import { PromptSelect } from './components/prompt-select';
-import { useState } from 'react';
+
 
 export function App() {
   const [temperature, setTemperature] = useState(0.5)
   const [videoId, setVideoId] = useState<string | null>(null)
 
-
-  function handlePromptSelected(template: string) {
-    console.log(template)
-  }
-
-
+  const {
+    input,
+    setInput,
+    handleInputChange
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+  })
 
   return (
     <div className='min-h-screen flex flex-col'>
@@ -49,6 +56,8 @@ export function App() {
           <div className='grid grid-rows-2 gap-4 flex-1'>
             <Textarea className='resize-none p-4 leading-relaxed'
               placeholder='Inclua o prompt para a IA...'
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea className='resize-none p-4 leading-relaxed'
               placeholder='Resultado gerado pela IA...' 
@@ -69,7 +78,7 @@ export function App() {
           <form className='space-y-6'>
             <div className='space-y-2'>
               <Label>Prompt</Label>
-              <PromptSelect onPromptSelected={handlePromptSelected} />
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className='space-y-2'>
